b19d00c37724386d9f44ba8fc0b936adfb8f1e48
GabbrielLps <jhosegabriel95@gmail.com>
2024-05-12 20:18:36 -0300

    feat: cria estrutura projeto
    feat: cria estrutura projeto

---
+++ src/test/java/com/gabbriellps/resttemplate/api/RestTemplateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabbriellps.resttemplate.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
